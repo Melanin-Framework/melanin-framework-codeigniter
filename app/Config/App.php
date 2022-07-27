f0b4a06ecc71aeded1e56da6d69a32a324f9d368
Aaron Will Djaba <papiliocurtis@gmail.com>
2022-07-27 09:32:53 +0000

    Fixed uri issue :bug:

--- app/Config/App.php
+++ app/Config/App.php
@@ -21,7 +21,7 @@ class App extends BaseConfig
 	| environments.
 	|
 	*/
-	public $baseURL = 'http://localhost/melanin-framework-codeigniter/';
+	public $baseURL = 'http://localhost/melanin-framework-codeigniter-bw/';
 
 	/*
 	|--------------------------------------------------------------------------
